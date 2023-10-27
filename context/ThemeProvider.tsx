b7ca7591b5a7bca518456d64effc441b9f5ddb99
altanakdar <altanakdar@gmail.com>
2023-10-27 18:22:30 +0300

    ride side bars

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -13,12 +13,12 @@ export function ThemeProvider({children}: {children: React.ReactNode}){
     const [mode, setMode] = useState('');
 
     const HandleThemeChange = () => {
-        if(mode === 'dark'){
-            setMode('light');
-            document.documentElement.classList.add('light');
-        }else {
+        if(localStorage.theme === 'dark' || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)){
             setMode('dark');
             document.documentElement.classList.add('dark');
+        }else {
+            setMode('light');
+            document.documentElement.classList.remove('dark');
         }
     }
     useEffect(() => {
